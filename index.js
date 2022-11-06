acb8c2dcaa58cd27731c32036f032ab021a9819c
saklainMostake <saklain15-12003@diu.edu.bd>
2022-11-07 01:11:57 +0600

    push

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require('express')
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
 const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+// dbUser UDza22vJMrCc1OAl
+
+
+
+const uri = "mongodb+srv://dbUser:UDza22vJMrCc1OAl@cluster0.ckrddue.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 async function run(){
     try{
-
-
+        const userCollection = client.db('nodeMongoCrud').collection('users')
+       app.post('/users', async (req,res) =>{
+        const user = req.body
+        const result = await userCollection.insertOne(user)
+        res.send(result)
+       })
     }
     finally{
 
